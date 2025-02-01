466ed2eab39f4a6a873ebcc05f137f1855ac3fd3
Bruno Ćorić <bruno.coric13@gmail.com>
2025-02-01 12:10:40 +0100

    credentials

--- nogomet_app/gdrive_setup.py
+++ nogomet_app/gdrive_setup.py
@@ -1,36 +1,17 @@
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
 import streamlit as st
+from pydrive2.auth import GoogleAuth, ServiceAccountCredentials
+from pydrive2.drive import GoogleDrive
 import json
-import os
 import pandas as pd
-
 DRIVE_ID = "16D6QN5nrtsmBy7rRtYQf46gpUiytWALu"
 
-
-
 def get_drive_instance():
-    if 'drive' not in st.session_state:
-        # Create a temporary credentials file
-        creds_json = json.loads(st.secrets["GDRIVE_CREDENTIALS"])
-
-        with open("temp_creds.json", "w") as f:
-            json.dump(creds_json, f)
-
-
-        # Initialize GoogleAuth
-        gauth = GoogleAuth()
-        # Disable automatic webserver authentication
-        gauth.settings['get_refresh_token'] = False
-
-        # Set up service account authentication
-        gauth.ServiceAuth()
-
-        # Create and store GoogleDrive instance
-        st.session_state.drive = GoogleDrive(gauth)
-
+    # Initialize Google Drive connection if not already done
+    scope = ['https://www.googleapis.com/auth/drive']
+    creds_json = json.loads(st.secrets["GDRIVE_CREDENTIALS"])
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
+    st.session_state.drive = GoogleDrive(credentials)
     return st.session_state.drive
-
 
 def save_csv_to_drive(df, filename) -> str:
     drive = get_drive_instance()
@@ -39,13 +20,11 @@ def save_csv_to_drive(df, filename) -> str:
     file.Upload()
     return file['id']
 
-
 def load_csv_from_drive(file_id, file_name) -> pd.DataFrame:
     drive = get_drive_instance()
     file = drive.CreateFile({'id': file_id})
     file.GetContentFile(file_name)
     return pd.read_csv(file_name)
-
 
 def list_csvs_in_folder() -> dict[str, str]:
     drive = get_drive_instance()
