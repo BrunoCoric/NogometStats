b6387216b68d66e8659ed35bafc0bdc20ce4814f
Bruno Ćorić <bruno.coric13@gmail.com>
2025-02-01 12:06:52 +0100

    credentials

--- nogomet_app/gdrive_setup.py
+++ nogomet_app/gdrive_setup.py
@@ -4,26 +4,31 @@ from pydrive2.drive import GoogleDrive
 import json
 import pandas as pd
 
-scope = ['https://www.googleapis.com/auth/drive']
-
-creds_json = json.loads(st.secrets["GDRIVE_CREDENTIALS"])
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
-
-drive = GoogleDrive(credentials)
-
 DRIVE_ID = "16D6QN5nrtsmBy7rRtYQf46gpUiytWALu"
 
+def get_drive_instance():
+    # Initialize Google Drive connection if not already done
+    if 'drive' not in st.session_state:
+        scope = ['https://www.googleapis.com/auth/drive']
+        creds_json = json.loads(st.secrets["GDRIVE_CREDENTIALS"])
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
+        st.session_state.drive = GoogleDrive(credentials)
+    return st.session_state.drive
+
 def save_csv_to_drive(df, filename) -> str:
+    drive = get_drive_instance()
     file = drive.CreateFile({'title': filename, 'parents': [{'id': DRIVE_ID}]})
     file.SetContentString(df.to_csv(index=False))
     file.Upload()
-    return file['id']  # Returns the file ID
+    return file['id']
 
 def load_csv_from_drive(file_id, file_name) -> pd.DataFrame:
+    drive = get_drive_instance()
     file = drive.CreateFile({'id': file_id})
     file.GetContentFile(file_name)
     return pd.read_csv(file_name)
 
 def list_csvs_in_folder() -> dict[str, str]:
+    drive = get_drive_instance()
     file_list = drive.ListFile({'q': f"'{DRIVE_ID}' in parents and mimeType='text/csv'"}).GetList()
     return {file['title']: file['id'] for file in file_list}
